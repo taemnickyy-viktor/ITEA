607ec4eab4ce91b51c952e873e77ab215b5e77fd
taemnickyy.viktor <taemnickyy.viktor@gmail.com>
2019-08-19 13:59:34 +0300

    [Added classes structure]

--- Boxers_vtaemnic.py
+++ Boxers_vtaemnic.py
@@ -1,3 +1,4 @@
+from random import randint
 class Peoples:
     def __init__(self, name, age, weight):
         self.name = name  #Full name of the person
@@ -10,28 +11,54 @@ class Boxers(Peoples):
         #set health of boxer
         self.health = health
 
-    def hit(self, hit):
-        if hit == 1:
-            self.jab()
-        elif hit == 2:
-            self.cross()
-        elif hit == 3:
-            self.left_hook()
-        elif hit == 4:
-            self.right_hook()
+    def get_health(self):
+        return self.health
 
+    def hit_move(self, hit):
+        if hit == 1:
+            return self.jab()
+        elif hit == 2:
+            return self.cross()
+        elif hit == 3:
+            return self.left_hook()
+        elif hit == 4:
+            return self.right_hook()
+        return hit
+
+    def defense_move(self, dfnd):
+        if dfnd == 1:
+            self.left_block()
+        elif dfnd == 2:
+            self.right_block()
+        elif dfnd == 3:
+            self.left_dodge()
+        elif dfnd == 4:
+            self.right_dodge()
+        elif dfnd == 5:
+            self.left_dive()
+        elif dfnd == 6:
+            self.right_dive()
+        return dfnd
+
+    #Attach functions
     def jab(self):
-        #light damage left hand front hit
+        #light damage left hand front hit (defence - right_dodge)
         print('jab')
+        return randint(4, 8)
     def cross(self):
-        #middle damage right hand front hit
+        #middle damage right hand front hit (defence - left_dodge)
         print('cross')
+        return randint(8, 12)
     def left_hook(self):
-        #powerful left hand side hit
+        #powerful left hand side hit (defence - right_dive)
         print('left_hook')
+        return randint(12, 16)
     def right_hook(self):
-        #powerful right hand side hit
+        #powerful right hand side hit (defence - left_dive)
         print('right_kook')
+        return randint(16, 20)
+
+    #Defend functions
     def left_block(self):
         #defence against cross and right_hit. Reduce damage from hit to 20%
         print('left_block')
@@ -39,36 +66,64 @@ class Boxers(Peoples):
         # defence against jab and left_hit. Reduce damage from hit to 20%
         print('right_block')
     def left_dodge(self):
-        #defence against cross and jab. In case of cross it is counterattack possible - left_hook
+        #defence against cross.
+        #TODO In case of cross it is counterattack possible - left_hook
         print('left_dodge')
     def right_dodge(self):
-        #defence against cross and jab. In case of jab it is counterattack possible - right_hook
+        #defence against jab.
+        #TODO In case of jab it is counterattack possible - right_hook
         print('right_dodge')
     def left_dive(self):
-        #defence against right_hook. Counterattack possible - right_hook
+        #defence against right_hook.
+        #TODO Counterattack possible - right_hook
         print('left_dive')
     def right_dive(self):
-        #defence against left_hook. Counterattack possible - left_hook
+        #defence against left_hook.
+        #TODO Counterattack possible - left_hook
         print('right_dive')
 
-class Fight:
-    def __init__(self, place, char1, char2):
-        self.place = place
-        self.char1 = char1
-        self.char2 = char2
+class Fight():
+    def __init__(self, boxer1, boxer2):
+        # self.boxer1 = boxer1
+        # self.boxer2 = boxer2
+        print('-= Fight has begun =-!')
+
+    def player_move(self, boxer1, boxer2):
+        hit = int(input('1. Jab, 2. Cross, 3. Left hook, 4. Right hook: '))
+        dfnc = boxer2.defense_move(randint(1, 4))
+        dmg_whole = boxer1.hit_move(hit)
+        print(dmg_whole, 'damage caused')
+        if 1 <= hit <= 4 and 1 <= dfnc <= 2:
+            dmg = round(dmg_whole * 0.8, 2)
+            print('Damage caused:', dmg, '(blocked -20% from)', dmg_whole)
+            boxer2.set_health(boxer2.get_health() - dmg)
+            print(boxer2.get_health())
+        elif hit == 1 and dfnc == 4:
+            print('jab dodjed!')
+        elif hit == 2 and dfnc == 3:
+            print('cross dodjed')
+        elif hit == 3 and dfnc == 6:
+            print('left hook dived!')
+        elif hit == 4 and dfnc == 5:
+            print('right hook dived!')
 
 
 def main():
-    import random
     boxer1 = Boxers('Marco Orlando', 40, 75)
     boxer2 = Boxers('Enrico Polanci', 30, 90)
     boxer1.set_health(100)
     boxer2.set_health(100)
+    main_fight = Fight(boxer1, boxer2)
 
     while boxer1.health > 0 or boxer2.health > 0:
-        boxer1.hit(int(input('1. Jab, 2. Cross, 3. Left hook, 4. Right hook')))
-        boxer2.hit(random.randint(1,4))
-
+        # cur_hit = boxer1.hit_move(int(input('1. Jab, 2. Cross, 3. Left hook, 4. Right hook: ')))
+        # cur_def = boxer2.defense_move(randint(1, 4))
+        # main_fight.player_move(cur_hit, cur_def, boxer1)
+        # boxer1.defense_move(int(input('1. Left block, 2. Right block, 3. Left dodge, 4. Right dodge, 5. Left dive, 6. Right dive: ')))
+        # boxer2.hit_move(randint(1, 4))
+        # main_fight.player_move(cur_hit, cur_def)
+        # main_fight.test()
+        main_fight.player_move(boxer1, boxer2)
 
 
 
