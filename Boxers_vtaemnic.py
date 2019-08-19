ccb5cc5afc46ab602705db229741c4ee4a060b4d
taemnickyy.viktor <taemnickyy.viktor@gmail.com>
2019-08-19 18:18:04 +0300

    [Fixed some bugs. Colors added]

--- Boxers_vtaemnic.py
+++ Boxers_vtaemnic.py
@@ -1,31 +1,35 @@
 from random import randint
+from colorama import Fore, Style
 class Peoples:
-    def __init__(self, name, age, weight):
+    def __init__(self, name):
         self.name = name  #Full name of the person
-        self.age = age  # older boxers lost more health
-        self.weight = weight  # heavy boxers cause more damage
+        #TODO
+        # self.age = age  # older boxers lost more health
+        # self.weight = weight  # heavy boxers cause more damage
 
 
 class Boxers(Peoples):
     def set_health(self, health):
-        #set health of boxer
+        #Set health of boxer
         self.health = health
 
     def get_health(self):
+        #Function for health status check
         return self.health
 
-    def hit_move(self, name, hit):
-        if hit == 1:
+    def hit_move(self, name, cur_hit):
+        #Chose current attach move
+        if cur_hit == 1:
             return self.jab(name)
-        elif hit == 2:
+        elif cur_hit == 2:
             return self.cross(name)
-        elif hit == 3:
+        elif cur_hit == 3:
             return self.left_hook(name)
-        elif hit == 4:
+        elif cur_hit == 4:
             return self.right_hook(name)
-        return hit
 
     def defense_move(self, name, dfnd):
+        #Chose current defence move
         if dfnd == 1:
             self.block(name)
         elif dfnd == 2:
@@ -41,7 +45,7 @@ class Boxers(Peoples):
     #Attach functions
     def jab(self, name):
         #light damage left hand front hit (defence - right_dodge)
-        print(name, ' do Jab!')
+        print(name, 'do Jab!')
         return randint(4, 8)
     def cross(self, name):
         #middle damage right hand front hit (defence - left_dodge)
@@ -79,57 +83,67 @@ class Boxers(Peoples):
 
 class Fight():
     def __init__(self):
-        print('-= Fight has begun =-!')
+        print(Fore.RED + '-= The Fight has Begun! =-' + Style.RESET_ALL)
 
     def player_move(self, boxer1, boxer2):
-        hit = int(input('Player hit: 1. Jab, 2. Cross, 3. Left hook, 4. Right hook: '))
-        dmg_whole = boxer1.hit_move(boxer1.name, hit)
-        dfnc = boxer2.defense_move(boxer2.name, randint(1, 5))
-        self.calculate_damage(hit, dmg_whole, dfnc, boxer1, boxer2)
+        #This is the function for Player move
+        cur_pl_hit = int(input('%s%s%s attack move: 1. Jab, 2. Cross, 3. Left hook, 4. Right hook: ' % (Fore.GREEN, boxer1.name, Style.RESET_ALL)))
+        cur_pl_dmg_whole = boxer1.hit_move(boxer1.name, cur_pl_hit)
+        cur_pl_dfnc = boxer2.defense_move(boxer2.name, randint(1, 5))
+        self.calculate_damage(cur_pl_hit, cur_pl_dmg_whole, cur_pl_dfnc, boxer1, boxer2)
 
     def opponent_move(self, boxer1, boxer2):
+        #This is the function for computer move
         if boxer2.health <= 0:
             return 1 #opponent in knockout
-        dfnc = boxer1.defense_move(boxer1.name, int(input('Player defence: 1. Block, 2. Left dodge, 3. Right dodge, 4. Left dive, 5. Right dive: ')))
-        hit = (randint(1, 4))
-        dmg_whole = boxer2.hit_move(boxer2.name, hit)
-        self.calculate_damage(hit, dmg_whole, dfnc, boxer2, boxer1)
+        cur_op_dfnc = boxer1.defense_move(boxer1.name, int(input('%s%s%s defence move: 1. Block, 2. Left dodge, 3. Right dodge, 4. Left dive, 5. Right dive: ' % (Fore.GREEN, boxer1.name, Style.RESET_ALL))))
+        cur_op_hit = (randint(1, 4))
+        cur_op_dmg_whole = boxer2.hit_move(boxer2.name, cur_op_hit)
+        self.calculate_damage(cur_op_hit, cur_op_dmg_whole, cur_op_dfnc, boxer2, boxer1)
         return 0 #oponent sill have health
 
     def calculate_damage(self, hit, dmg_whole, dfnc, boxer1, boxer2):
+        #This function calculates damage according of defence move
         if dfnc == 1:
             dmg = round(dmg_whole * 0.5, 2)
             boxer2.set_health(round(boxer2.get_health() - dmg, 2))
-            print("%s cause %s damage (blocked 50%s out of %s). %s health [%s]" % (boxer1.name, dmg, '%', dmg_whole, boxer2.name, boxer2.get_health()))
+            print("%s%s%s caused %s%s%s damage (blocked 50%s out of %s). %s health %s[%s]%s" % (Fore.GREEN, boxer1.name, Fore.RESET, Fore.RED, dmg, Style.RESET_ALL, '%', dmg_whole, boxer2.name, Fore.BLUE, boxer2.get_health(), Style.RESET_ALL))
         elif hit == 1 and dfnc == 3:
-            print("%s missed! %s has dodged from jab! %s health [%s]" % (boxer1.name, boxer2.name, boxer2.name, boxer2.get_health()))
+            print("%s%s%s has missed! %s has dodged from jab! %s health [%s]" % (Fore.GREEN, boxer1.name, Fore.RESET, boxer2.name, boxer2.name, boxer2.get_health()))
         elif hit == 2 and dfnc == 2:
-            print("%s missed! %s has dodged from cross! %s health [%s]" % (boxer1.name, boxer2.name, boxer2.name, boxer2.get_health()))
+            print("%s%s%s has missed! %s has dodged from cross! %s health [%s]" % (Fore.GREEN, boxer1.name, Fore.RESET, boxer2.name, boxer2.name, boxer2.get_health()))
         elif hit == 3 and dfnc == 5:
-            print("%s missed! %s has dived from left hook! %s health [%s]" % (boxer1.name, boxer2.name, boxer2.name, boxer2.get_health()))
+            print("%s%s%s has missed! %s has dived from left hook! %s health [%s]" % (Fore.GREEN, boxer1.name, Fore.RESET, boxer2.name, boxer2.name, boxer2.get_health()))
         elif hit == 4 and dfnc == 4:
-            print("%s missed! %s has dived from right hook! %s health [%s]" % (boxer1.name, boxer2.name, boxer2.name, boxer2.get_health()))
+            print("%s%s%s has missed! %s has dived from right hook! %s health [%s]" % (Fore.GREEN, boxer1.name, Fore.RESET, boxer2.name, boxer2.name, boxer2.get_health()))
         else:
             boxer2.set_health(boxer2.get_health() - dmg_whole)
-            print("%s cause %s damage! %s health [%s]" % (boxer1.name, dmg_whole, boxer2.name, boxer2.get_health()))
+            print("%s%s%s caused %s damage! %s health [%s]" % (Fore.GREEN, boxer1.name, Fore.RED, dmg_whole, boxer2.name, boxer2.get_health()))
+
+    def check_results(self, boxer1, boxer2):
+        if boxer1.health <= 0 and boxer2.health <= 0:
+            print('What a fight! Looks like we have a draw! %s and %s knocked out! Unbelievable!' % (boxer1.name, boxer2.name))
+        elif boxer1.health <= 0:
+            print('Knockout! %s%s%s knocked out! 5..4..3..2..1..%s LOST! The winner is %s%s%s. Congratulations!' % (Fore.GREEN, boxer1.name, Fore.RESET, boxer1.name, Fore.GREEN, boxer2.name, Fore.RESET))
+        elif boxer2.health <= 0:
+            print('%s%s%s knocked out! 5..4..3..2..1..%s LOST! The winner is %s%s%s. Congratulations!' % (Fore.GREEN, boxer2.name, Fore.RESET, boxer2.name, Fore.GREEN, boxer1.name, Fore.RESET))
 
 def main():
-    boxer1 = Boxers('Marco Orlando', 40, 75)
-    boxer2 = Boxers('Enrico Polanci', 30, 90)
-    boxer1.set_health(100)
-    boxer2.set_health(100)
-    main_fight = Fight()
+    try:
+        boxer1 = Boxers(str(input('Enter player name: ')))
+        boxer2 = Boxers(str(input('Enter computer name: ')))
+        boxer1.set_health(100)
+        boxer2.set_health(100)
+        main_fight = Fight()
 
-    while boxer1.health > 0 and boxer2.health > 0:
-        main_fight.player_move(boxer1, boxer2)
-        if main_fight.opponent_move(boxer1, boxer2):
-            print('Knockout!!!')
-    if boxer1.health <= 0 and boxer2.health <= 0:
-        print('What a fight! Looks like we have a draw! %s and %s knocked out! Unbelievable!' % (boxer1.name, boxer2.name))
-    elif boxer1.health <= 0:
-        print('Knockout! %s knocked out! 5..4..3..2..1..%s LOST! The winner is %s. Congratulations!' % (boxer1.name, boxer1.name, boxer2.name))
-    elif boxer2.health <= 0:
-        print('%s knocked out! 5..4..3..2..1..%s LOST! The winner is %s. Congratulations!' % (boxer2.name, boxer2.name, boxer1.name))
+        while boxer1.health > 0 and boxer2.health > 0:
+            main_fight.player_move(boxer1, boxer2)
+            if main_fight.opponent_move(boxer1, boxer2):
+                print('Knockout!!!')
+        main_fight.check_results(boxer1, boxer2)
+
+    except TypeError:
+        print('Error: Invalid input data. Exit the program...')
 
 
 if __name__ == '__main__':
